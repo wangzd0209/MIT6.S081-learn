fc8e1680d06af1dd46377e9dec80ff49cf92fd2a
wangzd0209 <wangzd0209@163.com>
2022-08-14 23:44:54 +0000

    finish 22-08-15

--- kernel/trap.c
+++ kernel/trap.c
@@ -67,7 +67,25 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if (r_scause() == 13 || r_scause() == 15){
+    //读取虚拟地址
+    uint64 fault_va = r_stval();
+    //
+    char *pa;
+    if (fault_va < p->sz && PGROUNDUP(p->trapframe->sp) - 1 < fault_va)
+    {
+      if((pa = kalloc()) == 0)
+        p->killed = 1;
+      memset(pa, 0, PGSIZE);
+      if(mappages(p->pagetable, PGROUNDDOWN(fault_va), PGSIZE, (uint64)pa, PTE_W|PTE_X|PTE_R|PTE_U) != 0){
+        kfree(pa);
+        p->killed = 1;
+      }
+    }
+    else{
+      p->killed = 1;
+    }
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
